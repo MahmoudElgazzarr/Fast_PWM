afc85dd5b9b8e66dad9b2d54839ea4eded738a56
Mahmoud Elgazzar <mahmoud.elgazzar@avelabs.com>
2019-02-21 11:30:20 +0200

    first init

---
+++ Fast_PWM_Timer1/main.c
@@ -0,0 +1,18 @@
+/*
+ * Fast_PWM_Timer1.c
+ *
+ * Created: 2/21/2019 11:28:25 AM
+ * Author : AVE-LAP-040
+ */ 
+
+#include <avr/io.h>
+
+
+int main(void)
+{
+    /* Replace with your application code */
+    while (1) 
+    {
+    }
+}
+
